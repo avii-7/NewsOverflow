4732657f2f65d43f222ca2f03af0ba64d0375c9d
Avii <avii.official00@gmail.com>
2021-09-04 01:18:01 +0530

    Cleared logs

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMvMgmh8nRY4uqm549JFPNfpJlurUfclU",
+  authDomain: "newsoverflow-avii.firebaseapp.com",
+  projectId: "newsoverflow-avii",
+  storageBucket: "newsoverflow-avii.appspot.com",
+  messagingSenderId: "20919526214",
+  appId: "1:20919526214:web:03079aa1d11a1471213b47",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
