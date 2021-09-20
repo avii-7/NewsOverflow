9076a2efcb98797975d5a16c2c97462964260ffa
Avii <avii.official00@gmail.com>
2021-09-20 22:28:15 +0530

    Api Key Issue fixed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import LoadingBar from "react-top-loading-bar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
-  const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+  // const apiKey = process.env.REACT_APP_NEWS_API_KEY;
+  const apiKey = "ff57f410982249fda2664634041ba5c3";
   const [progress, setProgress] = useState(25);
 
   const updateProgress = (progress) => {
@@ -13,26 +14,33 @@ export default function App() {
   };
 
   return (
-      <Router>
-        <Navbar />
-        <LoadingBar height={3} color="#f11946" progress={progress} onLoaderFinished={() => { setProgress(0); } } />
-        <Switch>
-          <Route exact path="/">
-            <News setProgress={updateProgress} key="general" apiKey={apiKey} country="in" category="general" />
-          </Route>
-          <Route exact path="/entertainment">
-            <News setProgress={updateProgress} key="entertainment" apiKey={apiKey} country="in" category="entertainment" />
-          </Route>
-          <Route exact path="/sports">
-            <News setProgress={updateProgress} key="sports" apiKey={apiKey} country="in" category="sports" />
-          </Route>
-          <Route exact path="/technology">
-            <News setProgress={updateProgress} key="technology" apiKey={apiKey} country="in" category="technology" />
-          </Route>
-          <Route exact path="/health">
-            <News setProgress={updateProgress} key="health" apiKey={apiKey} country="in" category="health" />
-          </Route>
-        </Switch>
-      </Router>
+    <Router>
+      <Navbar />
+      <LoadingBar
+        height={3}
+        color="#f11946"
+        progress={progress}
+        onLoaderFinished={() => {
+          setProgress(0);
+        }}
+      />
+      <Switch>
+        <Route exact path="/">
+          <News setProgress={updateProgress} key="general" apiKey={apiKey} country="in" category="general" />
+        </Route>
+        <Route exact path="/entertainment">
+          <News setProgress={updateProgress} key="entertainment" apiKey={apiKey} country="in" category="entertainment" />
+        </Route>
+        <Route exact path="/sports">
+          <News setProgress={updateProgress} key="sports" apiKey={apiKey} country="in" category="sports" />
+        </Route>
+        <Route exact path="/technology">
+          <News setProgress={updateProgress} key="technology" apiKey={apiKey} country="in" category="technology" />
+        </Route>
+        <Route exact path="/health">
+          <News setProgress={updateProgress} key="health" apiKey={apiKey} country="in" category="health" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
